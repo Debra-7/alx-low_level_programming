86b7ebc85162a4e5ddffbcf0248a6fc85b40dd2d
Debra-7 <ndukudebrah07@gmail.com>
2023-06-22 12:02:22 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ * followed by a new line
+ *
+ * Return: 0 to 9
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
